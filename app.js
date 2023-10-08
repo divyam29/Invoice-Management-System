2eeb743247af6f19820b02060b06f5c6167beb54
divyam29 <divyamjain29@gmail.com>
2023-10-08 22:15:33 +0530

    Added View Products Page

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-require('dotenv').config()
-const createError = require('http-errors')
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const passport = require('passport')
-const flash = require('connect-flash')
-const Category = require('./models/category')
-var MongoStore = require('connect-mongo')(session)
-const connectDB = require('./config/db')
-const favicon = require('serve-favicon'); 
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const Category = require('./models/category');
+var MongoStore = require('connect-mongo')(session);
+const connectDB = require('./config/db');
+const favicon = require('serve-favicon');
 
-const app = express()
-require('./config/passport')
+const app = express();
+require('./config/passport');
 
 // mongodb configuration
-connectDB()
+connectDB();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(favicon(__dirname + '/public/images/favicon-32x32.png')); 
+app.use(favicon(__dirname + '/public/images/favicon-32x32.png'));
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
@@ -40,75 +40,77 @@ app.use(
         //session expires after 3 hours
         cookie: { maxAge: 60 * 1000 * 60 * 3 },
     })
-)
-app.use(flash())
-app.use(passport.initialize())
-app.use(passport.session())
+);
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // global variables across routes
 app.use(async (req, res, next) => {
     try {
-        res.locals.login = req.isAuthenticated()
-        res.locals.session = req.session
-        res.locals.currentUser = req.user
-        const categories = await Category.find({}).sort({ title: 1 }).exec()
-        res.locals.categories = categories
-        next()
+        res.locals.login = req.isAuthenticated();
+        res.locals.session = req.session;
+        res.locals.currentUser = req.user;
+        const categories = await Category.find({}).sort({ title: 1 }).exec();
+        res.locals.categories = categories;
+        next();
     } catch (error) {
-        console.log(error)
-        res.redirect('/')
+        console.log(error);
+        res.redirect('/');
     }
-})
+});
 
 // add breadcrumbs
 get_breadcrumbs = function (url) {
     var rtn = [{ name: 'Home', url: '/' }],
         acc = '', // accumulative url
-        arr = url.substring(1).split('/')
+        arr = url.substring(1).split('/');
 
     for (i = 0; i < arr.length; i++) {
-        acc = i != arr.length - 1 ? acc + '/' + arr[i] : null
+        acc = i != arr.length - 1 ? acc + '/' + arr[i] : null;
         rtn[i + 1] = {
             name: arr[i].charAt(0).toUpperCase() + arr[i].slice(1),
             url: acc,
-        }
+        };
     }
-    return rtn
-}
+    return rtn;
+};
 app.use(function (req, res, next) {
-    req.breadcrumbs = get_breadcrumbs(req.originalUrl)
-    next()
-})
+    req.breadcrumbs = get_breadcrumbs(req.originalUrl);
+    next();
+});
 
 //routes config
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
 const pagesRouter = require('./routes/pages');
 const usersRouter = require('./routes/user');
+const productsRouter = require('./routes/products');
 
-app.use('/', indexRouter)
+app.use('/', indexRouter);
 app.use('/pages', pagesRouter);
 app.use('/user', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404))
-})
+    next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-})
+    res.status(err.status || 500);
+    res.render('error');
+});
 
-var port = process.env.PORT || 3000
-app.set('port', port)
+var port = process.env.PORT || 3000;
+app.set('port', port);
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-})
+    console.log(`Server running at http://localhost:${port}`);
+});
 
-module.exports = app
+module.exports = app;
--- routes/pages.js
+++ routes/pages.js
@@ -1,41 +1,39 @@
-const express = require('express')
-const csrf = require('csurf')
-const nodemailer = require('nodemailer')
-const router = express.Router()
-
+const express = require('express');
+const csrf = require('csurf');
+const nodemailer = require('nodemailer');
+const router = express.Router();
 const {
     userContactUsValidationRules,
     validateContactUs,
-} = require('../config/validator')
-
-const csrfProtection = csrf()
-router.use(csrfProtection)
+} = require('../config/validator');
+const csrfProtection = csrf();
+router.use(csrfProtection);
 
 //GET: display abous us page
 router.get('/about-us', (req, res) => {
     res.render('pages/about-us', {
         pageName: 'About Us',
-    })
-})
+    });
+});
 
 //GET: display shipping policy page
 router.get('/shipping-policy', (req, res) => {
     res.render('pages/shipping-policy', {
         pageName: 'Shipping Policy',
-    })
-})
+    });
+});
 
 //GET: display contact us page and form with csrf tokens
 router.get('/contact-us', (req, res) => {
-    const successMsg = req.flash('success')[0]
-    const errorMsg = req.flash('error')
+    const successMsg = req.flash('success')[0];
+    const errorMsg = req.flash('error');
     res.render('pages/contact-us', {
         pageName: 'Contact Us',
         csrfToken: req.csrfToken(),
         successMsg,
         errorMsg,
-    })
-})
+    });
+});
 
 //POST: handle contact us form logic using nodemailer
 router.post(
@@ -48,14 +46,14 @@ router.post(
             port: 587,
             secure: false,
             auth: {
-                // email and password
+                // company's email and password
                 user: process.env.GMAIL_EMAIL,
                 pass: process.env.GMAIL_PASSWORD,
             },
             tls: {
                 rejectUnauthorized: false,
             },
-        })
+        });
 
         // email options
         const mailOpts = {
@@ -72,7 +70,7 @@ router.post(
       ${req.body.message}
       </div>
       `,
-        }
+        };
 
         // send the email
         smtpTrans.sendMail(mailOpts, (error, response) => {
@@ -80,17 +78,17 @@ router.post(
                 req.flash(
                     'error',
                     'An error occured... Please check your internet connection and try again later'
-                )
-                return res.redirect('/pages/contact-us')
+                );
+                return res.redirect('/pages/contact-us');
             } else {
                 req.flash(
                     'success',
                     'Email sent successfully! Thanks for your inquiry.'
-                )
-                return res.redirect('/pages/contact-us')
+                );
+                return res.redirect('/pages/contact-us');
             }
-        })
+        });
     }
-)
+);
 
-module.exports = router
+module.exports = router;
